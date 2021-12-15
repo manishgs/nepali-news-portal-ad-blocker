1ca0d6fccc17883fe4640019e71ff9e8124cfed4
Manish Gopal Singh <manish.singh707@gmail.com>
2021-12-15 22:32:26 +0545

    refactor code

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,37 @@
-$('#ok18-roadblock-wrap, .okam-device-desktop, .cube-bigyaapan, .box-wrapper, #content > div > div.row.center-aligned-ad, #roadblock-ad, .modalbox.desktop-jacket-bigyaapan, .news-whitebgcolor-popup, .modal-backdrop').remove();
+const domain = document.location.host.replace('www.','');
 
-$('body, html').find('a').each((i,e)=>{
-    const a = $(e);
-    const img = a.children('img').attr('src');
-    if(img && a.attr('href').includes('http') && !a.attr('href').includes(document.location.host.replace('www.','').replace('.com',''))){
-        a.remove();
-    }
-});
+const popup_removers = {
+    'onlinekhabar.com' : '#ok18-roadblock-wrap, .okam-device-desktop',
+    'setopati.com' : '.modalbox.desktop-jacket-bigyaapan',
+    'ratopati.com':'.news-whitebgcolor-popup, .box-wrapper.text-center, .modal-backdrop ',
+    'ekantipur.com' : '#roadblock-ad'
+}
+
+if(popup_removers[domain]){
+    $(popup_removers[domain]).remove();
+}
+
+if(domain === 'setopati.com'){
+    $('body').removeClass('modal-open');
+}
+
+if(domain === 'annapurnapost.com'){
+    $('.opac').on('DOMSubtreeModified', function(){
+        replaceImages();
+        $('.advertisement ').remove();
+    });
+}
+
+const replaceImages = function(){
+    $('body').find('a').each((i,e)=>{
+        const a = $(e);
+        const img = a.children('img').attr('src');
+        if(img && a.attr('href').includes('http') && !a.attr('href').includes(domain)){
+            a.remove();
+        }else if(img && img.endsWith('.gif')){
+            a.remove();
+        }
+    });    
+}
+
+replaceImages();
